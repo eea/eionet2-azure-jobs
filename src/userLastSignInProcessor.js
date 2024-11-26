bdf4759f4cb5f9a094027a07a0306a3696fe45cb
Mihai Nicolae <mihai.nicolae@antebit.com>
2024-11-26 09:40:51 +0200

    chore: format

--- src/userLastSignInProcessor.js
+++ src/userLastSignInProcessor.js
@@ -59,11 +59,11 @@ async function processUserLastSignIn(config) {
 
 async function loadUsers(listId) {
   let path = encodeURI(
-    auth.apiConfigWithSite.uri +
-    'lists/' +
-    listId +
-    '/items?$expand=fields&$top=999&$filter=fields/SignedIn eq 1',
-  ),
+      auth.apiConfigWithSite.uri +
+        'lists/' +
+        listId +
+        '/items?$expand=fields&$top=999&$filter=fields/SignedIn eq 1',
+    ),
     result = [];
 
   while (path) {
